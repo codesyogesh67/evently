dab1b26a70a0b30b984e34e0ff8aebc3d63f9a4d
yogesh <codes.yogesh67@gmail.com>
2024-01-10 10:26:19 -0500

    changing user model schema

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -11,7 +11,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
+    dbName: 'events',
     bufferCommands: false,
   })
 
